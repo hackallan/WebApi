bd1b73eafda6b93b32e6df3a8c5424f568c09b7c
qhackerjj@163.com <qhackerjj@163.com>
2016-12-29 21:52:35 +0800

    remove something

---
+++ WebApi/TEST/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TEST
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{Controller}/{Action}",
+                defaults: new { Controller = "Home", Action = "Index", end = UrlParameter.Optional }
+            );
+
+            routes.MapMvcAttributeRoutes();            
+
+        }
+    }
+}
---
+++ WebApi/TEST/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Routing;
+namespace TEST.Controllers
+{
+    [RoutePrefix("myhome")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("you")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("my")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
